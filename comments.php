836fb0330113a5d0e6e883a37efbc41080cef659
Jesus Federico <jesus@123it.ca>
2013-05-02 11:56:52 -0400

    Turned off messages related to comments when page is protected

--- comments.php
+++ comments.php
@@ -14,7 +14,7 @@
 ?>
 	<div id="comments">
 	<?php if ( post_password_required() ) : ?>
-		<p class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.', 'twentyeleven' ); ?></p>
+		<!-- <p class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.', 'twentyeleven' ); ?></p> -->
 	</div><!-- #comments -->
 	<?php
 			/* Stop the rest of comments.php from being processed,
